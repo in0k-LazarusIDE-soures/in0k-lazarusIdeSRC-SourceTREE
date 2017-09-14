68828dbc4f11f172ffad577514f1ac00ee5c277c
in0k <slvr9@mail.ru>
2017-09-14 23:09:27 +0500

    Обобщенный поиск: начало

--- FNKs/in0k_lazIdeSRC_srcTree_FNK_PATH_FND.pas
+++ FNKs/in0k_lazIdeSRC_srcTree_FNK_PATH_FND.pas
@@ -9,6 +9,8 @@ uses
   in0k_lazIdeSRC_srcTree_CORE_fileSystem_FNK,
   in0k_lazIdeSRC_srcTree_CORE_itemFileSystem,
   //---
+  in0k_lazIdeSRC_srcTree_item_Globals,
+  //---
   in0k_lazIdeSRC_srcTree_FNK_PATH_FND_rel,
   in0k_lazIdeSRC_srcTree_FNK_PATH_FND_abs;
 
@@ -18,6 +20,7 @@ implementation
 
 function SrcTree_fndPath(const item:tSrcTree_ROOT; const path:string):_tSrcTree_item_fsNodeFLDR_;
 var tmp:tSrcTree_item;
+    tmpAbsPATH:string;
 begin
     if srcTree_fsFnk_FilenameIsAbsolute(path) then begin
         result:=nil;
@@ -25,15 +28,16 @@ begin
         tmp:=item.ItemCHLD;
         while Assigned(tmp) do begin
             if tmp is _tSrcTree_item_fsNodeFLDR_ then begin
-                if (srcTree_fsFnk_CompareFilenames(path,_tSrcTree_item_fsNodeFLDR_(tmp).src_abs_PATH)=0)
+                tmpAbsPATH:=_tSrcTree_item_fsNodeFLDR_(tmp).src_abs_PATH;
+                if (srcTree_fsFnk_CompareFilenames(path,tmpAbsPATH)=0)
                 then begin //< нашли ПОЛНОЕ совпадение
                     result:=_tSrcTree_item_fsNodeFLDR_(tmp);
                     BREAK;
                 end
                else
-                if (srcTree_fsFnk_FileIsInPath(path,_tSrcTree_item_fsNodeFLDR_(tmp).src_abs_PATH)
+                if srcTree_fsFnk_FileIsInPath(path,tmpAbsPATH)
                 then begin //< tmp для нас РОДИТЕЛЬСКАЯ, поищем ВНУТРИ неё
-                    result:=SrcTree_fndPathREL(_tSrcTree_item_fsNodeFLDR_(tmp), srcTree_fsFnk_CreateRelativePath(path,_tSrcTree_item_fsNodeFLDR_(tmp).src_abs_PATH));
+                    result:=SrcTree_fndPathREL(_tSrcTree_item_fsNodeFLDR_(tmp), DirectorySeparator+srcTree_fsFnk_CreateRelativePath(path,tmpAbsPATH));
                     if Assigned(result) then begin
                         BREAK; //< таки НАШЛИ
                     end;
